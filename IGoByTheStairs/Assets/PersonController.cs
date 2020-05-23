8d64670a58ef7e314d92fac2fc203b8bf6cb6939
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-23 15:56:11 +0200

    The Person start moving

---
+++ IGoByTheStairs/Assets/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonController : MonoBehaviour
+{
+  private Vector2 direction;
+  public float velocity = 3f;
+  public Rigidbody2D theRG;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    direction = GetDirection();
+    Move();
+  }
+
+  void Move()
+  {
+    theRG.velocity = direction * velocity;
+  }
+
+  Vector2 GetDirection()
+  {
+    Vector2 result;
+
+    if (Input.GetKey(KeyCode.RightArrow))
+    {
+      result = new Vector2(1f, 0f);
+    }
+    else if (Input.GetKey(KeyCode.LeftArrow))
+    {
+      result = new Vector2(-1f, 0f);
+    }
+    else
+    {
+      result = Vector2.zero;
+    }
+
+    return result;
+  }
+}
